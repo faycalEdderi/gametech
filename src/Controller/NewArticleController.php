f2460676b67092f1e092f7594e0b03613e766f23
faycalEdderi <faycmx@live.fr>
2019-05-23 16:52:08 +0200

    ajouter un article + photo

---
+++ src/Controller/NewArticleController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticlesType;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class NewArticleController extends AbstractController
+{
+    /**
+     * @Route("admin/new/article", name="new_article")
+     */
+    public function index(Request $request, ObjectManager $objectManager, int $id = null, ArticleRepository $articleRepository):Response
+    {
+        $entity = $id ? $articleRepository->find($id) : new Article();
+        $type = ArticlesType::class;
+
+        $entity->prevImage = $entity->getImage();
+
+		//dd($entity);
+		$form = $this->createForm($type, $entity);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$entity->getId()){
+                $imageName = bin2hex(random_bytes(16));
+				$uploadedFile = $entity->getImage(); // renvoie un objet UploadedFile
+				$extension = $uploadedFile->guessExtension();
+                $uploadedFile->move('img/', "$imageName.$extension");
+                
+                $entity->setImage("$imageName.$extension");
+            }
+            elseif($entity->getId() && !$entity->getImage()){
+				// récupération de la propriété dynamique prevImage pour remplir la propriété image
+				$entity->setImage( $entity->prevImage );
+				//dd($entity);
+            }
+            elseif($entity->getId() && $entity->getImage()){
+				// unlink : suppression de l'ancienne image
+				unlink("img/{$entity->prevImage}");
+
+				// transfert de la nouvelle image
+				$imageName = bin2hex(random_bytes(16));
+				$uploadedFile = $entity->getImage();
+				$extension = $uploadedFile->guessExtension();
+				$uploadedFile->move('img/', "$imageName.$extension");
+				$entity->setImage("$imageName.$extension");
+			}
+       
+            $objectManager->persist($entity);
+			$objectManager->flush();
+ 
+            //$this->addFlash('notice', 'L\'article été ajouté');
+ 
+            return $this->redirectToRoute('accueil');
+         }
+
+        return $this->render('new_article/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+		$entity = $options['data'];
+		$constraintsImage = [
+			new Image([
+				'mimeTypes' => [ 'image/jpeg', 'image/gif', 'image/png', 'image/svg+xml', 'image/webp' ],
+				'mimeTypesMessage' => "Formats d'image acceptés: jpeg, gif, png, webp."
+			])
+		];
+		
+        $builder
+            ->add('titre', TextType::class, [
+            	'constraints' => [
+            		new NotBlank([
+            			'message' => "Veuillez saisir un titre"
+		            ]),
+		            new Length([
+		            	'min' => 2,
+			            'max' => 150,
+			            'minMessage' => 'Le titre de l\'article doit comporter au minimum {{ limit }} caractères',
+			            'maxMessage' => 'Le titre de l\'article {{ limit }} caractères',
+		            ])
+	            ]
+            ])
+            ->add('image', FileType::class, [
+            	'constraints' => $constraintsImage,
+	            'help' => 'Veuillez sélectionner une image au format JPG, GIF, PNG, SVG ou WebP',
+	            'data_class' => null
+            ])
+            ->add('texte', TextareaType::class, [
+            	'constraints' => [
+		            new NotBlank([
+			            'message' => "Veuillez saisir un texte"
+		            ]),
+		            new Length([
+			            'min' => 10,
+			            'minMessage' => 'Le texte doit comporter au minimum {{ limit }} caractères',
+		            ])
+	            ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
