24e9f5d892a3aea763f36d9c086ea845944814ab
faycalEdderi <faycmx@live.fr>
2019-06-08 13:23:00 +0200

    menu + footer + details article + categories

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -19,4 +19,60 @@ class PrincipalController extends AbstractController
            'afficher'=> $results
        ]);
     }
+
+    /**
+     * @Route("/nouveau", name="nouveau")
+     */
+    public function nouveau(ArticleRepository $articleRepository):Response
+    {
+       $results = $articleRepository->findByCategory("nouveaute");
+       return $this->render('categories/categorie.html.twig', [
+           'afficher'=> $results
+       ]);
+    }
+
+    /**
+     * @Route("/ps4", name="ps4")
+     */
+    public function ps4(ArticleRepository $articleRepository):Response
+    {
+       $results = $articleRepository->findByCategory("playstation 4");
+       return $this->render('categories/categorie.html.twig', [
+           'afficher'=> $results
+       ]);
+    }
+
+       /**
+     * @Route("/xbox", name="xbox")
+     */
+    public function xboxOne(ArticleRepository $articleRepository):Response
+    {
+       $results = $articleRepository->findByCategory("xbox one");
+       return $this->render('categories/categorie.html.twig', [
+           'afficher'=> $results
+       ]);
+    }
+
+       /**
+     * @Route("/pc", name="pc")
+     */
+    public function pc(ArticleRepository $articleRepository):Response
+    {
+       $results = $articleRepository->findByCategory("pc");
+       return $this->render('categories/categorie.html.twig', [
+           'afficher'=> $results
+       ]);
+    }
+
+      /**
+     * @Route("/article/{id}", name="article.details")
+     */
+    public function details(int $id, ArticleRepository $articleRepository):Response
+    {
+        $result = $articleRepository->find($id);
+        //dd($result);
+        return $this->render('principal/details.html.twig', [
+            'article' => $result,
+        ]);
+    }
 }
