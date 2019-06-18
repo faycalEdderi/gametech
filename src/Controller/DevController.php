1612ca9bd727289434fbc1f2b33882fce7a532a3
faycalEdderi <faycmx@live.fr>
2019-06-18 12:06:25 +0200

    modification user ok

--- src/Controller/DevController.php
+++ src/Controller/DevController.php
@@ -89,7 +89,6 @@ class DevController extends AbstractController
 
     /**
      * @Route("admin/new/user", name="new.user")
-     * @Route("/admin/updateUser/{id}", name="user.update")
      */
     public function newUser(Request $request, int $id = null, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder):Response
     {
@@ -119,6 +118,34 @@ class DevController extends AbstractController
          }
 
         return $this->render('dev/newUser.html.twig', [
+            'form' => $form->createView(),
+        ]);
+   
+    }
+    
+
+    /**
+     * @Route("/admin/updateUser/{id}", name="user.update")
+     */
+    public function updateUser(Request $request, int $id = null, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder):Response
+    {
+        $user = $id ? $userRepository->find($id) : new User();
+        $type = ModifUserType::class;
+
+        $form = $this->createForm(ModifUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+ 
+            return $this->redirectToRoute('user.dev');
+         }
+
+        return $this->render('dev/updateUser.html.twig', [
             'form' => $form->createView(),
         ]);
    
