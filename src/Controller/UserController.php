0827a670810a09e74db714a9bc5466dd341b8c1a
faycalEdderi <faycmx@live.fr>
2019-06-20 14:00:14 +0200

    utilisateur peuvent modifier profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 
+use App\Form\ModifUserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
@@ -18,6 +22,32 @@ class UserController extends AbstractController
         return $this->render('user/profile.html.twig');
     }
 
+    /**
+     * @Route("/user/modifierProfile/{id}", name="profile.update")
+     */
+    public function modifierProfile(Request $request, int $id = null, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder):Response
+    {
+        $user = $id ? $userRepository->find($id) : new User();
+        $type = ModifUserType::class;
+
+        $form = $this->createForm(ModifUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+ 
+            return $this->redirectToRoute('user.profile');
+         }
+
+        return $this->render('user/modifProfile.html.twig', [
+            'form' => $form->createView(),
+        ]);
+   
+    }
  
   
 }
