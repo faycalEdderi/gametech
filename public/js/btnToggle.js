d88bcf4a6af1965a84bd7f277fe8b48695e5c893
faycalEdderi <faycmx@live.fr>
2019-06-23 19:14:00 +0200

    button toggle administrateur

---
+++ public/js/btnToggle.js
@@ -0,0 +1,22 @@
+$('#togglePs4').click(function(){
+
+	$('.Playstation').toggle(1000);
+});
+
+$('#toggleXbox').click(function(){
+
+	$('.Xbox').toggle(1000);
+});
+
+$('#togglePc').click(function(){
+
+	$('.PC').toggle(1000);
+});
+
+
+
+
+$('#toggleSwitch').click(function(){
+
+	$('.nintendo').toggle(1000);
+});
