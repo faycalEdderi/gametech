1b7a3102af4778defa81a8eed036dfe33c75353c
faycalEdderi <faycmx@live.fr>
2019-07-01 20:00:49 +0200

    animation menu responsive

--- public/js/menuBurgerToggle.js
+++ public/js/menuBurgerToggle.js
@@ -2,13 +2,30 @@
 
 	$('#menuBurger').click(function(){
 
-		$('.menuResponsive').toggle(1000);
-		$('.user').toggle(1000);
+				$('.menuResponsive').toggle().finish();
+				$('.user').toggle().finish();
+
+				
+
+
+
+				
+			});
+
+	$(window).resize(function() {
+		if (window.matchMedia('(max-width: 1200px)').matches) {
+			// functionality for screens smaller than 1200px
+			$('.menuResponsive').hide(1000);
+			$('.user').hide(1000);
+			
+		}else{
+
+			$('.menuResponsive').show(1000);
+			$('.user').show(1000);
+
+		}
 	});
- 
 
 
 
-
-
-
+	
