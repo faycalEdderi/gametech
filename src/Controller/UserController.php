3edc73d8763d8fcb75e0a20651a69146e32b9cfc
faycalEdderi <faycmx@live.fr>
2019-07-09 14:55:08 +0200

    securisation delete modif account user et mdp

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -31,28 +31,31 @@ class UserController extends AbstractController
      */
     public function modifierProfile(Request $request, int $id = null, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder):Response
     {
-        $user = $id ? $userRepository->find($id) : new User();
-        $type = ModifUserType::class;
+        $userId = $this->getUser()->getId();
+        if($id == $userId ){
 
-        $form = $this->createForm(ModifUserType::class, $user);
-        $form->handleRequest($request);
+            $user = $id ? $userRepository->find($id) : new User();
+            $type = ModifUserType::class;
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $form = $this->createForm(ModifUserType::class, $user);
+            $form->handleRequest($request);
 
-            // message
-            $this->addFlash('notice', "Les informations ont été modifiés");
-            
-            return $this->redirectToRoute('user.profile');
-         }
+            if ($form->isSubmitted() && $form->isValid()) {
+                
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
 
-        return $this->render('user/modifProfile.html.twig', [
-            'form' => $form->createView(),
-        ]);
-   
+                // message
+                $this->addFlash('notice', "Les informations ont été modifiés");
+                
+                return $this->redirectToRoute('user.profile');
+            }
+
+            return $this->render('user/modifProfile.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
     }
 
     // suppression de compte utilisateur
@@ -61,22 +64,22 @@ class UserController extends AbstractController
 	 */
 	public function userDelete(int $id, UserRepository $userRepository, ObjectManager $objectManager, TokenStorageInterface $tokenStorage, SessionInterface $session):Response
 	{   
+        $userId = $this->getUser()->getId();
+                
+        if($id == $userId ){
+            // sélection de l'entité par son identifiant
+            $entity = $userRepository->find($id);
+            
+            // suppression de l'entité
+            $objectManager->remove($entity); 
+            $objectManager->flush();
+            $tokenStorage->setToken(null);
+            $session->invalidate();
 
-        
-    
-		// sélection de l'entité par son identifiant
-        $entity = $userRepository->find($id);
-        
-		// suppression de l'entité
-        $objectManager->remove($entity); 
-        $objectManager->flush();
-        $tokenStorage->setToken(null);
-        $session->invalidate();
-
-        // message
-        $this->addFlash('notice', "Votre compte a été supprimé");
-        return $this->redirectToRoute('accueil');
-        
+            // message
+            $this->addFlash('notice', "Votre compte a été supprimé");
+            return $this->redirectToRoute('accueil');
+        }
         
        
 		
@@ -90,42 +93,43 @@ class UserController extends AbstractController
      * @Route("/user/mdp/{id}", name="user.changeMdp")
      */
     public function changeMdp(Request $request, int $id = null, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder):Response
-    {
-        $user = $id ? $userRepository->find($id) : new User();
-        $type = ModifMdpType::class;
+    {   $userId = $this->getUser()->getId();
+        if($id == $userId ){
+            $user = $id ? $userRepository->find($id) : new User();
+            $type = ModifMdpType::class;
 
-        $form = $this->createForm(ModifMdpType::class, $user);
-        $form->handleRequest($request);
+            $form = $this->createForm(ModifMdpType::class, $user);
+            $form->handleRequest($request);
 
-       
+        
+        
+            if ($form->isSubmitted() && $form->isValid()) {
 
-        if ($form->isSubmitted() && $form->isValid()) {
 
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+                
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                
+                $entityManager->flush();
 
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('password')->getData()
-                )
-            );
-            
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            
-            $entityManager->flush();
+                // message
+                $this->addFlash('notice', "Le mot de passe a été modifié");
+                
+                return $this->redirectToRoute('user.profile');
+            }
 
-            // message
-            $this->addFlash('notice', "Le mot de passe a été modifié");
-            
-            return $this->redirectToRoute('user.profile');
-         }
-
-        return $this->render('user/modifMdp.html.twig', [
-            'form' => $form->createView(),
-        ]);
-   
-	}
-
+            return $this->render('user/modifMdp.html.twig', [
+                'form' => $form->createView(),
+            ]);
+    
+        }
+    }
  
   
 }
